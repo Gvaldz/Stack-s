577e70a3ec8f9a04fff8030341a4b41a7632c5e6
Gvaldz <152213250+Gvaldz@users.noreply.github.com>
2024-04-17 03:24:08 -0600

    Update index.js

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,8 +1,10 @@
-import { convert as infixToPrefix } from './converter.js';
+import { Converter } from './Converter.js';
+
+const infixToPrefix = new Converter();
 
 document.getElementById("btn").addEventListener("click", function() {
     const infixExpression = document.getElementById('infixExpression').value;
     const result = document.getElementById('result');
-    const prefixExpression = infixToPrefix(infixExpression);
+    const prefixExpression = infixToPrefix.convert(infixExpression);
     result.textContent = 'Prefija: ' + prefixExpression;
 });
